6e7e7a7b7daf3b4a0956dde973dec9aa4d32c629
HaiPaya <haipaya@dzd.tj.cn>
2026-02-05 19:07:16 +0800

    feat(menu): 添加互通菜单功能

--- src/main/kotlin/cn/tj/dzd/mc/dzt/menu/be/Main.kt
+++ src/main/kotlin/cn/tj/dzd/mc/dzt/menu/be/Main.kt
@@ -10,11 +10,11 @@ fun openMainBEMenu(pl: Player, fpl: FloodgatePlayer) {
     fpl.sendForm(SimpleForm.builder()
         .title("§l§bD§cZ§bD§dG§ea§bm§ce §6主菜单")
         .button("传送菜单", FormImage.Type.PATH, "textures/ui/mashup_world.png")
+        .button("互通菜单", FormImage.Type.PATH, "textures/ui/classrooms_icon.png")
         .validResultHandler({
             when (it.clickedButtonId()) {
-                0 -> {
-                    openTeleportBEMenu(pl, fpl)
-                }
+                0 -> { openTeleportBEMenu(pl, fpl) }
+                1 -> { openGeyserBEMenu(pl, fpl) }
             }
         })
     )
